153dfbe5ffeec42b326a2058ca42a744a7eea0ff
ravi <samcollins9899@gmail.com>
2023-04-15 11:12:34 +0530

    initial run

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,24 @@
 import express from 'express';
+import http from 'http';
+import bodyparser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+    credentials: true
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyparser.json());
+
+const server = http.createServer(app);
+server.listen(5000, ()=> {
+    console.log("server running on 5000");
+});
 
 
-console.log("hello node");
+
