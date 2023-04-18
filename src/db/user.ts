dde4f1c429d79eb841eba8459bbd7f18ca16d176
ravi <samcollins9899@gmail.com>
2023-04-18 07:50:49 +0530

    added helpers

--- src/db/user.ts
+++ src/db/user.ts
@@ -1,31 +1,34 @@
 import mongoose, { mongo } from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    username: {type: String, required: true},
-    email: {type:String, required: true},
+  username: { type: String, required: true },
+  email: { type: String, required: true },
 
-    authentication:{
-        password: {
-            type: String,
-            required:true,
-            select: false
-        },
-       salt:  { type:String, select:false},
-       sessionToken: {
-        type: String, select:false
-       },
-
-    }
-})
-
-export const UserModel = mongoose.model('User', userSchema);
-
-export const getUserEmail= (email: string)=> UserModel.findOne({email});
-export const getUserBySessionToken =(sessionToken: string)=> UserModel.findOne({
-    'authentication.sessionToken': sessionToken,
+  authentication: {
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    salt: { type: String, select: false },
+    sessionToken: {
+      type: String,
+      select: false,
+    },
+  },
 });
 
-export const getUserById = (id: string) => UserModel.findById(id);   
-export const deleteUserById = (id: string) => UserModel.findOneAndDelete({ _id: id});
+export const UserModel = mongoose.model("User", userSchema);
 
-export const updateUserById =(id: string,values : Record<string, any>) => UserModel.findByIdAndUpdate(id,values);
+export const getUserEmail = (email: string) => UserModel.findOne({ email });
+export const getUserBySessionToken = (sessionToken: string) =>
+  UserModel.findOne({
+    "authentication.sessionToken": sessionToken,
+  });
+
+export const getUserById = (id: string) => UserModel.findById(id);
+export const deleteUserById = (id: string) =>
+  UserModel.findOneAndDelete({ _id: id });
+
+export const updateUserById = (id: string, values: Record<string, any>) =>
+  UserModel.findByIdAndUpdate(id, values);
