9a3b62caccce023bb067fe8bd8e13c49412bc679
sky <you@example.com>
2023-04-18 15:19:44 +0530

    added routes and fixes

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -19,6 +19,7 @@ export const register = async(req: express.Request, res: express.Response)=>{
         }
 
         const salt = random();
+        console.log(random);
         const user = await createUser({
             email,
             username,
