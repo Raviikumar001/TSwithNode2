fb140ce0a8f8dc1467878972b5608c880fd45947
ravi <samcollins9899@gmail.com>
2023-04-20 08:37:12 +0530

    added login and register routes

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,7 +1,49 @@
 import express from 'express';
 
-import { getUserEmail, createUser } from '../db/user';
+import { getUserByEmail, createUser } from '../db/user';
 import { random, authentication } from '../helpers';
+
+export const login = async (req: express.Request, res: express.Response)=>{
+
+    try{
+
+        const {email, password} = req.body;
+
+        if(!email || !password)
+        {
+            return  res.sendStatus(400);
+        }
+
+        const user = await getUserByEmail(email).select('+authentication.salt +authentication.password');
+        if(!user)
+        { 
+            return res.sendStatus(400);
+
+        }
+
+        const expectedHash = authentication(user.authentication.salt, password);
+
+        if(user.authentication.password !== expectedHash)
+        {
+            return res.sendStatus(403);
+        }
+
+        const salt = random();
+        user.authentication.sessionToken = authentication(salt, user._id.toString());
+
+        await user.save();
+        res.cookie(
+            'RAVI-AUTH', user.authentication.sessionToken, {domain: 'localhost', path : '/'}
+        )
+
+        return res.status(200).json(user).end();
+        
+    }catch(error)
+    {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
 
 export const register = async(req: express.Request, res: express.Response)=>{
 
@@ -12,7 +54,7 @@ export const register = async(req: express.Request, res: express.Response)=>{
             return res.sendStatus(400);
         }
 
-        const existingUser = await getUserEmail(email);
+        const existingUser = await getUserByEmail(email);
 
         if(existingUser){
             return res.sendStatus(400);
--- src/db/user.ts
+++ src/db/user.ts
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema({
 export const UserModel = mongoose.model("User", userSchema);
 
 export const getUsers = ()=> UserModel.find();
-export const getUserEmail = (email: string) => UserModel.findOne({ email });
+export const getUserByEmail = (email: string) => UserModel.findOne({ email });
 export const getUserBySessionToken = (sessionToken: string) =>
   UserModel.findOne({
     "authentication.sessionToken": sessionToken,
